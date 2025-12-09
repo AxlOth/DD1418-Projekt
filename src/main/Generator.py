ae246549fde0f23f6ff66468fd0d1d8b56cb03ea
AxlOth <148064123+AxlOth@users.noreply.github.com>
2025-12-09 12:40:49 +0100

    Update Generator.py

---
+++ src/main/Generator.py
@@ -0,0 +1,142 @@
+import math
+import argparse
+import codecs
+from collections import defaultdict
+import random
+
+"""
+This file is part of the computer assignments for the course DD1418 at KTH.
+"""
+
+class Generator(object) :
+    """
+    This class generates words from a language model.
+    """
+    def __init__(self):
+    
+        # The mapping from words to identifiers.
+        self.index = {}
+
+        # The mapping from identifiers to words.
+        self.word = {}
+
+        # An array holding the unigram counts.
+        self.unigram_count = {}
+
+        # The bigram log-probabilities.
+        self.bigram_prob = defaultdict(dict)
+
+        # Number of unique words (word forms) in the training corpus.
+        self.unique_words = 0
+
+        # The total number of words in the training corpus.
+        self.total_words = 0
+
+        # The average log-probability (= the estimation of the entropy) of the test corpus.
+        # Important that it is named self.logProb for the --check flag to work
+        self.logProb = 0
+
+        # The identifier of the previous word processed in the test corpus. Is -1 if the last word was unknown.
+        self.last_index = -1
+
+        # The fraction of the probability mass given to unknown words.
+        self.lambda3 = 0.000001
+
+        # The fraction of the probability mass given to unigram probabilities.
+        self.lambda2 = 0.01 - self.lambda3
+
+        # The fraction of the probability mass given to bigram probabilities.
+        self.lambda1 = 0.99
+
+        # The number of words processed in the test corpus.
+        self.test_words_processed = 0
+
+
+    def read_model(self,filename):
+        """
+        Reads the contents of the language model file into the appropriate data structures.
+
+        :param filename: The name of the language model file.
+        :return: True if the entire file could be processed, False otherwise.
+        """
+
+        try:
+            with codecs.open(filename, 'r', 'utf-8') as f:
+                self.unique_words, self.total_words = map(int, f.readline().strip().split(' '))
+
+                # read all words and unigram counts
+                for i in range(self.unique_words):
+                    index, word, count = f.readline().strip().split(' ')
+                    self.index[word] = index
+                    self.word[index] = word
+                    self.unigram_count[index] = count
+
+                line = f.readline().strip()
+                while (line != "-1"):
+
+                    index1, index2, logProb = line.split(' ')
+                    self.bigram_prob[index1][index2] = logProb
+                    line = f.readline().strip()
+
+
+                    
+                return True
+        except IOError:
+            print("Couldn't find bigram probabilities file {}".format(filename))
+            return False
+
+    def generate(self, w):
+        """
+        Generates and prints n words, starting with the word w, and following the distribution
+        of the language model.
+        """ 
+
+        # print("words:")
+        # print(self.index.items())
+
+        # print("\nbigrams:")
+        # print(self.bigram_prob.items())
+
+        
+        if w in self.index and self.index[current_word] in self.bigram_prob:
+            rand = random.uniform(0,1)
+
+            current_sum = 0
+            for next_word_index, logprob in self.bigram_prob[self.index[current_word]].items():
+                # print(f"chcecking {current_word}, {next_word_index}!")
+
+                current_sum += math.exp(float(logprob))
+                if rand <= current_sum:
+                    next_word = self.word[next_word_index]
+                    print(" ",next_word, end="")
+                    current_word = next_word
+                    break
+
+        else: 
+            # print(f"word {current_word} not found in bigrams!")
+            current_word = random.choice(list(self.word.values()))
+            print(" ",current_word, end="")
+                
+
+
+
+
+        pass
+
+def main():
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(description='BigramTester')
+    parser.add_argument('--file', '-f', type=str,  required=True, help='file with language model')
+    parser.add_argument('--start', '-s', type=str, required=True, help='starting word')
+    parser.add_argument('--number_of_words', '-n', type=int, default=100)
+
+    arguments = parser.parse_args()
+
+    generator = Generator()
+    generator.read_model(arguments.file.strip())
+    generator.generate(arguments.start.strip(),arguments.number_of_words)
+
+if __name__ == "__main__":
+    main()
